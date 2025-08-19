4a97a32f905655ce6da881291842d1f0b449e85e
luis manuel <luismanuel.rosario.sssm6a@gmail.com>
2025-08-19 11:39:39 -0400

    Agregar archivos de proyecto.

---
+++ Clases_Kiss/Aplicando_KISS.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principio_Kiss.Clases_Kiss
+{
+    internal class Aplicando_KISS
+    {
+
+        static void Main(string[] args)
+        {
+            CalculateTotal();
+        }
+
+        static void CalculateTotal()
+        {
+            Console.WriteLine("Ingrese los precios de los platos (separados por comas): ");
+            string[] partes = Console.ReadLine()!.Split(',');
+
+            decimal subtotal = 0;
+            foreach (var parte in partes)
+            {
+                subtotal += decimal.Parse(parte);
+            }
+
+            Console.WriteLine("¿Propina personalizada? (s/n): ");
+            decimal propina = 0.10m;
+            if (Console.ReadLine()!.ToLower() == "s")
+            {
+                Console.WriteLine("Total a pagar: " + (subtotal + subtotal * propina));
+            }
+            else
+            {
+                Console.WriteLine("Total a pagar: " + (subtotal + subtotal * propina / 100));
+            }
+
+           
+        }
+    }
+}
